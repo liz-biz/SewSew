8bcf5763984fd5dbdd5b8811fefedf9d1a110925
Elizabeth <elizabela232@gmail.com>
2024-04-16 18:31:37 -0400

    got rid of extra, uneeded files

--- SewSew/SewSew/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SewSew.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
