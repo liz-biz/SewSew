f828a6442355271c18e139795e0484c044d3f078
Elizabeth <elizabela232@gmail.com>
2024-04-17 21:41:50 -0400

    The new project- added the template connected it to index.
    cant connect correctly to other view pages... its not outputting the correct template style. Will move on to implementing entity and repo pattern

--- Sew-Sew/Program.cs
+++ Sew-Sew/Program.cs
@@ -1,17 +1,7 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using SewSew.Areas.Identity.Data;
-using SewSew.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContextConnection' not found.");
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -34,8 +24,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages();
-
 app.Run();
-
 //hey there
