b5fadcd37180c39b5b790c81902bd4e8bb3a6faf
Elizabeth <elizabela232@gmail.com>
2024-10-12 17:46:56 -0400

    login/register pages: created the password eye-icon, and toggle funct.

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -159,11 +159,43 @@ $(document).ready(function(){
     $('section').closest('body').find('.close-iframe').click(function() {
         $(this).closest('.list-menu').toggleClass('reveal-modal');
     });
-    
-
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
 
   });
 
+/* Register/Login -----------------------------------*/
+
+//password-toggle 
+$(document).ready(function () {
+    //register password
+    const togglePassword = $('#toggle-password');
+    const passwordInput = $('#password-input');
+
+    togglePassword.on('click', function () {
+        const type = passwordInput.attr('type') === 'password' ? 'text' : 'password';
+        passwordInput.attr('type', type);
+        $(this).toggleClass('bi-eye-fill bi-eye-slash-fill');
+    });
+
+    //register confirm-password
+    const toggleConfirmPassword = $('#toggle-confirm-password');
+    const confirmPasswordInput = $('#confirm-password-input');
+
+    toggleConfirmPassword.on('click', function () {
+        const type = confirmPasswordInput.attr('type') === 'password' ? 'text' : 'password';
+        confirmPasswordInput.attr('type', type);
+        $(this).toggleClass('bi-eye-fill bi-eye-slash-fill');
+    });
+
+    //login password
+    const toggleLoginPassword = $('#toggle-login-password');
+    const loginPasswordInput = $('#login-password-input');
+
+    toggleLoginPassword.on('click', function () {
+        const type = loginPasswordInput.attr('type') === 'password' ? 'text' : 'password';
+        loginPasswordInput.attr('type', type);
+        $(this).toggleClass('bi-eye-fill bi-eye-slash-fill');
+    });
+});
